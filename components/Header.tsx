0f94bda5ac0a95dcbade9f873c8ef7dd4de63f19
levblanc <levblanc@gmail.com>
2022-10-07 17:00:56 +0800

    Style `Header` component with Tailwind

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,14 +3,17 @@ import { ConnectButton } from 'web3uikit';
 
 const Header = () => {
   return (
-    <nav>
-      <Link href="/">
-        <a>NFT Marketplace</a>
-      </Link>
-      <Link href="/sell-nft">
-        <a>Sell NFT</a>
-      </Link>
-      <ConnectButton />
+    <nav className="p-5 border-b-2 flex flex-row justify-between items-center">
+      <h1 className="px-4 py-4 font-bold text-3xl">NFT Marketplace</h1>
+      <div className="flex flex-row items-center">
+        <Link href="/">
+          <a className="mr-4 p-5">Home</a>
+        </Link>
+        <Link href="/sell-nft">
+          <a className="mr-4 p-5">Sell NFT</a>
+        </Link>
+        <ConnectButton moralisAuth={false} />
+      </div>
     </nav>
   );
 };
