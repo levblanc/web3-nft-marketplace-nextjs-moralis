d0b9360c251fa880be745614610bf781106168e4
levblanc <levblanc@gmail.com>
2022-10-08 10:22:32 +0800

    Add moralis server configs & yarn script

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,9 @@ import Head from 'next/head';
 import { MoralisProvider } from 'react-moralis';
 import Header from '../components/Header';
 
+const APP_ID = process.env.NEXT_PUBLIC_APP_ID!;
+const SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL!;
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <div>
@@ -12,7 +15,7 @@ function MyApp({ Component, pageProps }: AppProps) {
         <meta name="description" content="NFT Marketplace" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <MoralisProvider initializeOnMount={false}>
+      <MoralisProvider appId={APP_ID} serverUrl={SERVER_URL}>
         <Header />
         <Component {...pageProps} />
       </MoralisProvider>
